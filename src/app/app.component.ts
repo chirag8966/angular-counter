bef8a580ba732ebd24adf792e71347fd742b14de
chiragAnimatrics <62055879+chiragAnimatrics@users.noreply.github.com>
2020-09-12 20:55:58 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'counter-app';
+  title = 'Counter App';
+  count = 0;
+
+
+  increaseCount(): void {
+    this.count += 1;
+  }
+
+  decreaseCount(): void {
+    if (this.count !== 0 ) {
+      this.count -= 1;
+    }
+  }
+
+  // this is the arrow function syntax
+  // both syntax are absolutely fine just keep consistency
+  resetCount = () => {
+    this.count = 0;
+  }
 }
